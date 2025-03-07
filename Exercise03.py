612e20aee7126e609846dc5c6ec2a818d3e8d7fb
britishterminator <Aliaaryan66@gmail.com>
2025-03-08 02:47:07 +0400

    Exercise 1: Coding is Cool

---
+++ Exercise03.py
@@ -0,0 +1,22 @@
+User_Data = {"name":"Ali Aaryan",
+             "home town":"Delhi",
+             "age":"20"}
+print("Name:", User_Data["name"])
+print("Home town:", User_Data["home town"])
+print("Age:", User_Data["age"])
+
+
+
+
+
+
+
+First_Name = input("Enter your First Name: ")
+Last_Name = input("Enter your Last Name: ")
+Home_town = input("Enter your Home Town: ")
+Age = int(input("Enter your Age: "))
+print("Name:" , First_Name , Last_Name)
+print("Home Town:" , Home_town)
+print("age:" , Age)
+
+
